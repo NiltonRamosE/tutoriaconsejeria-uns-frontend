1dc9d0e299e924eb21ed3976c62efd3960b2469b
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-07-20 16:18:37 -0500

    feat: Add site configuration with navigation items

---
+++ src/shared/site.ts
@@ -0,0 +1,33 @@
+export const siteConfig = {
+  name: "Tutoría y Consejería EPISI",
+  description:
+    "La tutoría y consejería es un servicio que brinda la Universidad Nacional del Santa",
+  navItems: [
+    {
+      label: "Docentes",
+      href: "/docentes",
+    },
+    {
+      label: "Tutoría",
+      href: "/tutoria",
+    },
+    {
+      label: "Nosotros",
+      href: "/nosotros",
+    },
+  ],
+  navMenuItems: [
+    {
+      label: "Docentes",
+      href: "/docentes",
+    },
+    {
+      label: "Tutoría",
+      href: "/tutoria",
+    },
+    {
+      label: "Nosotros",
+      href: "/nosotros",
+    },
+  ],
+};
