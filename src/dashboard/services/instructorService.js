04501863f45c179d86bc3e68da5624b0930466a1
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-08-25 18:04:57 -0500

    feat: implement endpoint for appointments sent in instructor service

--- src/dashboard/services/instructorService.js
+++ src/dashboard/services/instructorService.js
@@ -52,3 +52,20 @@ export async function submitGroupAppointment(data, token) {
         throw error;
     }
 }
+
+// Función para cargar citas enviadas por el instructor
+export async function fetchAppointmentsSent(instructorId, token) {
+    try {
+        const response = await fetch(`http://localhost:8080/instructor/appointments/sent/${instructorId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        if (!response.ok) throw new Error(response.statusText);
+        return await response.json();
+    } catch (error) {
+        console.error('Error al cargar las citas enviadas por el instructor:', error);
+        throw error;
+    }
+}
