0f8791c56e43a5a82c56515f8cb6e6b8deabf0ea
NiltonRamosE <niltonencarnacion17@gmail.com>
2025-08-26 18:58:22 -0500

    feat: add endpoint appointments received in student service

--- src/dashboard/services/studentService.js
+++ src/dashboard/services/studentService.js
@@ -103,3 +103,20 @@ export async function fetchAppointmentsSent(studentId, token) {
         throw error;
     }
 }
+
+// Función para cargar citas recibidas por el estudiante
+export async function fetchAppointmentsReceived(studentId, token) {
+    try {
+        const response = await fetch(`http://localhost:8080/student/appointments/received/${studentId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${token}`
+            }
+        });
+        if (!response.ok) throw new Error(response.statusText);
+        return await response.json();
+    } catch (error) {
+        console.error('Error al cargar las citas recibidas por el estudiante:', error);
+        throw error;
+    }
+}
